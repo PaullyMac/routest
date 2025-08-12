e79a8e3d71d5ce9e58625e031159a200a847e99c
sykeruzn <syruzkendomingo.c@gmail.com>
2025-08-12 22:44:55 +0800

    Modify migration for route requests

--- backend/laravel/database/migrations/2025_08_12_144349_create_route_requests_table.php
+++ backend/laravel/database/migrations/2025_08_12_144349_create_route_requests_table.php
@@ -4,22 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('route_requests', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->uuid('id')->primary();
+            $table->uuid('origin_id');
+            $table->json('stops');
+            $table->timestamp('request_time')->useCurrent();
+            $table->string('status')->default('pending');
+
+            $table->foreign('origin_id')->references('id')->on('locations')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('route_requests');
