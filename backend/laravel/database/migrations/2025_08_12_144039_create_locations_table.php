1ecd0d2b8f871b0764c3d5483ce8ee7097bcafb5
sykeruzn <syruzkendomingo.c@gmail.com>
2025-08-12 22:43:12 +0800

    Modify migration for locations table

--- backend/laravel/database/migrations/2025_08_12_144039_create_locations_table.php
+++ backend/laravel/database/migrations/2025_08_12_144039_create_locations_table.php
@@ -5,20 +5,17 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('locations', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->decimal('latitude', 9, 6);
+            $table->decimal('longitude', 9, 6);
+            $table->timestamp('created_at')->useCurrent();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('locations');
