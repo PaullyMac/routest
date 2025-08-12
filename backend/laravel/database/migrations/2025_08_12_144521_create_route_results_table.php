81d4641c864a616779df1f3b45205e693e116f96
sykeruzn <syruzkendomingo.c@gmail.com>
2025-08-12 22:46:44 +0800

    Modify migration for route results

--- backend/laravel/database/migrations/2025_08_12_144521_create_route_results_table.php
+++ backend/laravel/database/migrations/2025_08_12_144521_create_route_results_table.php
@@ -5,20 +5,21 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('route_results', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->uuid('id')->primary();
+            $table->uuid('request_id');
+            $table->json('optimized_order');
+            $table->decimal('total_distance', 10, 2)->nullable();
+            $table->decimal('total_duration', 10, 2)->nullable();
+            $table->json('legs');
+            $table->timestamp('created_at')->useCurrent();
+
+            $table->foreign('request_id')->references('id')->on('route_requests')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('route_results');
