ba7196e2d470c63317fe8032e15ced944f838893
sykeruzn <syruzkendomingo.c@gmail.com>
2025-08-13 22:03:22 +0800

    Add locations route for checking data fetch from Supabase

--- backend/laravel/routes/api.php
+++ backend/laravel/routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Location;
+
+Route::get('/locations', function () {
+    return Location::on('pgsql')->get();
+});
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
